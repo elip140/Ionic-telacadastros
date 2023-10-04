115a387c3853f326a13c2fd3d2f5f88cb8083389
elip140 <epcotrim@gmail.com>
2023-10-04 14:51:42 -0300

    a

--- telaprimeiroacesso/src/app/home/home.page.ts
+++ telaprimeiroacesso/src/app/home/home.page.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { PrimeiroacessoPage } from '../primeiroacesso/primeiroacesso.page';
+
 
 @Component({
   selector: 'app-home',
@@ -6,7 +8,9 @@ import { Component } from '@angular/core';
   styleUrls: ['home.page.scss'],
 })
 export class HomePage {
-
-  constructor() {}
+  component = PrimeiroacessoPage;
+  constructor() {
+    
+  }
 
 }
--- telaprimeiroacesso/src/app/primeiroacesso/primeiroacesso.page.ts
+++ telaprimeiroacesso/src/app/primeiroacesso/primeiroacesso.page.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild, ElementRef } from '@angular/core';
 
 @Component({
   selector: 'app-primeiroacesso',
@@ -6,10 +6,61 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./primeiroacesso.page.scss'],
 })
 export class PrimeiroacessoPage implements OnInit {
+  Rg:any = "";
+  Nome:any = "";
+  Email:any = "";
+  Senha:any = "";
+  ConfSenha:any = "";
 
-  constructor() { }
+  rgCount:number = 0;
+  ishidden:boolean = true;
 
-  ngOnInit() {
+  constructor(){}
+
+  ngOnInit() {}
+
+  // Botão para enviar
+  OnClickSubmit() {
+    let info = {
+      Rg: this.Rg,
+      Nome: this.Nome,
+      Email: this.Email,
+      Senha: this.Senha,
+      ConfSenha: this.ConfSenha
+    };
+    alert(JSON.stringify(info));
   }
 
+  // Funções para os Inputs
+  OnKeyUpRg(event: any){
+    this.Rg = event.target.value;
+    this.rgCount = event.target.value.length;
+
+    if(this.rgCount>=9){
+      this.ishidden = false;
+    }
+    else{
+      this.ishidden = true;
+    }
+  }
+
+  OnKeyUpNome(event: any){
+    this.Nome = event.target.value;
+  }
+
+  OnKeyUpEmail(event: any){
+    this.Email = event.target.value;
+  }
+
+  OnKeyUpSenha(event: any){
+    this.Senha = event.target.value;
+  }
+
+  OnKeyUpConfSenha(event: any){
+    this.ConfSenha = event.target.value;
+  }
+  
+
 }
+
+
