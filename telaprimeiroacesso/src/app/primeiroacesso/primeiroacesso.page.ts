d22e401c53d3bb3277b1af7a3c86098bb7297426
elip140 <epcotrim@gmail.com>
2023-10-04 17:15:22 -0300

    Formulario

--- telaprimeiroacesso/src/app/primeiroacesso/primeiroacesso.page.ts
+++ telaprimeiroacesso/src/app/primeiroacesso/primeiroacesso.page.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit, ViewChild, ElementRef } from '@angular/core';
+import { FormBuilder, FormGroup, Validators, FormControl } from '@angular/forms';
+import { AlertController } from '@ionic/angular';
 
 @Component({
   selector: 'app-primeiroacesso',
@@ -6,34 +8,49 @@ import { Component, OnInit, ViewChild, ElementRef } from '@angular/core';
   styleUrls: ['./primeiroacesso.page.scss'],
 })
 export class PrimeiroacessoPage implements OnInit {
-  Rg:any = "";
-  Nome:any = "";
-  Email:any = "";
-  Senha:any = "";
-  ConfSenha:any = "";
+
+  public formPAcesso: FormGroup = this.formBuilder.group({
+    Rg: new FormControl('testedenome', Validators.compose([
+      Validators.required
+    ])),
+    Nome: new FormControl('', Validators.compose([
+      Validators.required
+    ])),
+    Email: new FormControl('', Validators.compose([
+      Validators.required
+    ])),
+    Senha: new FormControl('', Validators.compose([
+      Validators.required
+    ])),
+    ConfSenha: new FormControl('', Validators.compose([
+      Validators.required
+    ]))
+  });
 
   rgCount:number = 0;
-  ishidden:boolean = true;
+  ishidden:boolean = false;
 
-  constructor(){}
+  public isSubimitted:boolean = false;
+
+  constructor(private formBuilder:FormBuilder, private alertController:AlertController){
+  }
+
 
   ngOnInit() {}
 
   // Botão para enviar
-  OnClickSubmit() {
-    let info = {
-      Rg: this.Rg,
-      Nome: this.Nome,
-      Email: this.Email,
-      Senha: this.Senha,
-      ConfSenha: this.ConfSenha
-    };
-    alert(JSON.stringify(info));
+  async OnClickSubmit() {
+    this.isSubimitted = true;
+
+    if(!this.formPAcesso.valid)
+      return;
+
+    alert(JSON.stringify(this.formPAcesso.value));
+    
   }
 
   // Funções para os Inputs
   OnKeyUpRg(event: any){
-    this.Rg = event.target.value;
     this.rgCount = event.target.value.length;
 
     if(this.rgCount>=9){
@@ -44,6 +61,7 @@ export class PrimeiroacessoPage implements OnInit {
     }
   }
 
+  /*(ionInput)="OnKeyUpConfSenha($event)"
   OnKeyUpNome(event: any){
     this.Nome = event.target.value;
   }
@@ -58,6 +76,26 @@ export class PrimeiroacessoPage implements OnInit {
 
   OnKeyUpConfSenha(event: any){
     this.ConfSenha = event.target.value;
+  }*/
+
+  public validation_messages = {
+    Rg: [
+      {type:'required', message:'(Rg é requirido)'}
+    ],
+    Nome: [
+      {type:'required', message:'(Nome é requirido)'}
+    ],
+    Email: [
+      {type:'required', message:'(Email é requirido)'},
+      {type:'special', message:'(O E-mail deve ser cadastrado, entre em contato com a Administração)'}
+    ],
+    Senha: [
+      {type:'required', message:'(Senha é requirida)'}
+    ],
+    ConfSenha: [
+      {type:'required', message:'(Confirmação de Senha é requirida)'}
+    ]
+
   }
   
 
